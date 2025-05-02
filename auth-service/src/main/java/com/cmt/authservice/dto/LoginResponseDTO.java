c2adc5d0359aea36b33eeae7339d07f491723574
MandydnaM <mandystudy928@gmail.com>
2025-05-02 23:27:39 +0200

    add api-gateway and auth-service

---
+++ auth-service/src/main/java/com/cmt/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.cmt.authservice.dto;
+
+public class LoginResponseDTO {
+  private final String token;
+
+  public LoginResponseDTO(String token) {
+    this.token = token;
+  }
+
+  public String getToken() {
+    return token;
+  }
+}
---
+++ auth-service/src/main/java/com/cmt/authservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cmt.authservice.repository;
+
+import com.cmt.authservice.model.User;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+  Optional<User> findByEmail(String email);
+}
